fcd48259c3a1725f96287f7bc48a110c68d55762
Kenneth Morris <Kmorris1217@gmail.com>
2022-06-22 23:18:03 -0400

    started code to check for config file

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 
 
+def f_run_bot():
+    print("Trading bot started!")
+    f_check_for_config()
+
+
+def f_check_for_config():
+    print("Checking for config file!")
+
+
 def main():
-    print("")
+    f_run_bot()
 
 
 if __name__ == "__main__":
